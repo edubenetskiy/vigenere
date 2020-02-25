a5a469f130af928a60959908d47cface6fb62c45
Egor Dubenetskiy <egor@banka.space>
2020-02-26 00:24:25 +0300

    Add dummy CoincidenceCounter

---
+++ src/main/java/space/banka/ifmo/infosec/vigenere/core/usecases/coincidence/CoincidenceCounter.java
@@ -0,0 +1,18 @@
+package space.banka.ifmo.infosec.vigenere.core.usecases.coincidence;
+
+public class CoincidenceCounter {
+
+    /**
+     * Calculates the index of coincidence for the specified string.
+     *
+     * <p>The <em>index of coincidence</em>, or IC for short, is a probability
+     * of drawing two matching letters by randomly selecting two characters
+     * from a given text.
+     *
+     * @param string the string for which the IC is computed.
+     * @return the IC for the given string.
+     */
+    double countIndexOfCoincidence(CharSequence string) {
+        throw new UnsupportedOperationException("Not implemented"); // TODO
+    }
+}
