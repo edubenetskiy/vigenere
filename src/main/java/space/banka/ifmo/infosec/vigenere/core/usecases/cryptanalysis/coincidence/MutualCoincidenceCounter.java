889629d57b7ace2c25a87d55aa4baee0cb0d97ce
Egor Dubenetskiy <egor@banka.space>
2020-02-26 04:12:30 +0300

    Add dummy MutualCoincidenceCounter

---
+++ src/main/java/space/banka/ifmo/infosec/vigenere/core/usecases/cryptanalysis/coincidence/MutualCoincidenceCounter.java
@@ -0,0 +1,19 @@
+package space.banka.ifmo.infosec.vigenere.core.usecases.cryptanalysis.coincidence;
+
+public class MutualCoincidenceCounter {
+
+    /**
+     * Calculates the index of mutual coincidence for the two specified strings.
+     *
+     * <p>The <em>index of mutual coincidence</em>, or IMC for short,
+     * is a probability of drawing two matching letters by randomly
+     * selecting one character from each of the two given texts.
+     *
+     * @param strA the first string for which the IMC is computed.
+     * @param strB the second string for which the IMC is computed.
+     * @return the IMC for the two given strings.
+     */
+    double computeIndexOfMutualCoincidence(CharSequence strA, CharSequence strB) {
+        throw new UnsupportedOperationException("Not implemented"); // TODO
+    }
+}
