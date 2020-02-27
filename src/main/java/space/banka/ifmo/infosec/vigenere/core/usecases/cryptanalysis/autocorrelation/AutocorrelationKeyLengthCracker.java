329c8d0db03d4068286013e5af7c5755ca9ee6ac
Egor Dubenetskii <egor.dubenetskii@sbb.ch>
2020-02-28 00:16:26 +0300

    Implement autocorrelation method for cracking key length

---
+++ src/main/java/space/banka/ifmo/infosec/vigenere/core/usecases/cryptanalysis/autocorrelation/AutocorrelationKeyLengthCracker.java
@@ -0,0 +1,35 @@
+package space.banka.ifmo.infosec.vigenere.core.usecases.cryptanalysis.autocorrelation;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class AutocorrelationKeyLengthCracker {
+
+    private static final int MIN_KEY_LENGTH = 3;
+    private static final int MAX_KEY_LENGTH = 25;
+
+    private final AutocorrelationCounter autocorrelationCounter = new AutocorrelationCounter();
+
+    public int crackKeyLength(CharSequence ciphertext) {
+        Optional<AbstractMap.SimpleEntry<Integer, Double>> maxAutocorrelation =
+                IntStream.range(MIN_KEY_LENGTH, MAX_KEY_LENGTH)
+                        .parallel()
+                        .mapToObj(keyLength -> {
+                            double autocorrelation = autocorrelationCounter.computeAutocorrelation(ciphertext, keyLength);
+                            return new AbstractMap.SimpleEntry<>(keyLength, autocorrelation);
+                        })
+                        .peek(entry -> {
+                            Integer keyLength = entry.getKey();
+                            Double autocorrelation = entry.getValue();
+                            System.out.println("For key length " + keyLength + ", " +
+                                               "autocorrelation coefficient is " + autocorrelation);
+                        })
+                        .max(Map.Entry.comparingByValue());
+
+        return maxAutocorrelation
+                .orElseThrow(() -> new IllegalStateException("unreachable"))
+                .getKey();
+    }
+}
---
+++ src/test/java/space/banka/ifmo/infosec/vigenere/core/usecases/cryptanalysis/autocorrelation/AutocorrelationKeyLengthCrackerTest.java
@@ -0,0 +1,24 @@
+package space.banka.ifmo.infosec.vigenere.core.usecases.cryptanalysis.autocorrelation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutocorrelationKeyLengthCrackerTest {
+
+    @Test
+    void returns_key_length_with_highest_autocorrelation() {
+        var ciphertext =
+                "СГУЧПВЭЛЛЗИРТЕПНДНФГИНБОРГЙУГЛЧД" +
+                "КОТХЖГУУМЗДХРЪСГСЮДТПЪАРВЙГГИЩВЧ" +
+                "ЭЕЦСТУЖВСЕВХАХЯФБЬБЕТФЗСЭФТХЖЗБЗ" +
+                "ЪГФБЩИХХРИПЖТЗВТЖЙТГОЙБНТФФЕОИХТ" +
+                "ТЕГИИОКЗПТФЛЕУГСФИПТЬМОФОКСХМГБТ" +
+                "ЖФЫГУЧОЮНФНШЗГЭЛЛШРУДЕНКОЛГГНСБК" +
+                "ССЕУПНФЦЕЕЕГГСЖНОЕЫИОНРСИТКЦЬЕДБ" +
+                "УБТЕТЛОТБФЦСБЮЙПМПЗТЖПТУФКДГ";
+
+        int estimatedKeyLength = new AutocorrelationKeyLengthCracker().crackKeyLength(ciphertext);
+        assertEquals(6, estimatedKeyLength);
+    }
+}
